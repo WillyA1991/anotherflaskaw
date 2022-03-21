2a6e4eacab73baaca944ee4c3c602d90a12507c1
Austin <awilson9654@gmail.com>
2022-03-21 14:17:52 -0400

    created and cross-checked the base code from KWilliams for mult, add, and sub across calculator, calculations, and operations

---
+++ calculator/operations/__init__.py
@@ -0,0 +1,29 @@
+""" These are the Operation classes """
+
+
+class Addition:
+    # Operations addition class
+
+    @staticmethod
+    def add(value_1, value_2):
+        # Add method
+        return value_1 + value_2
+
+
+class Subtraction:
+    # Operations subtraction class
+
+    @staticmethod
+    def subtract(value_1, value_2):
+        # Subtraction method
+        return value_1 - value_2
+
+
+class Multiplication:
+    # Operations multiplication class
+
+    @staticmethod
+    def multiply(value_1, value_2):
+        # Multiplication method
+        return value_1 * value_2
+
