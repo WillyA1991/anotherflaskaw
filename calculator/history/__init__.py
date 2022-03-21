8a66aeae6d5af48e54880bdda1680fb07ab323e0
Austin <awilson9654@gmail.com>
2022-03-21 16:21:54 -0400

    Added and cross-checked history tests from base code and made minor correction to calculator>history file

--- calculator/history/__init__.py
+++ calculator/history/__init__.py
@@ -1,5 +1,5 @@
 """ Calculation history class for maintaining result posterity """
-from calculator.operations import Addition, Subtraction, Multiplication
+from calculator.calculations import Addition, Subtraction, Multiplication
 
 
 class Calculations:
