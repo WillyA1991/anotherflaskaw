4f78782568854bae96e316e42ee2c29be9e9e613
Austin <awilson9654@gmail.com>
2022-03-21 10:02:26 -0400

    Corrected test descriptions for calculator and fixed Project 1 nav bar highlight issue

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -3,19 +3,19 @@ from calculator import Calculator
 
 
 def test_calculator_is_instance():
-    """Testing the Calculator"""
+    """Testing the instantiation of the Calculator"""
     calculator = Calculator()
     assert isinstance(calculator, Calculator)
 
 
 def test_calculator_get_result_method():
-    """Testing the Calculator"""
+    """Testing the get_result method for Calculator"""
     calculator = Calculator()
     assert calculator.get_result() == 0
 
 
 def test_calculator_result_property():
-    """Testing the Calculator"""
+    """Testing the result property of the Calculator"""
     calc1 = Calculator()
     calc2 = Calculator()
     calc1.result = 5
@@ -25,11 +25,12 @@ def test_calculator_result_property():
 
 
 def test_calculator_add_method():
-    """Testing the Calculator"""
+    """Testing the add method of the Calculator"""
     calculator = Calculator()
     assert calculator.add(1) == 1
 
+
 def test_calculator_subtract_method():
-    """Testing the Calculator Subtract"""
+    """Testing the subtract method of the Calculator"""
     calculator = Calculator()
     assert calculator.subtract(1) == -1
