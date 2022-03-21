57b7e4f5c553b89138c0edab7468b24e238e6f98
Austin <awilson9654@gmail.com>
2022-03-21 17:13:15 -0400

    created calculator>operations Division class and adjusted tests to handle that

--- tests/operations_test.py
+++ tests/operations_test.py
@@ -1,5 +1,5 @@
 """ Testing the calculator operations """
-from calculator.operations import Addition, Subtraction, Multiplication
+from calculator.operations import Addition, Subtraction, Multiplication, Division
 
 
 def test_calculator_operations_add():
@@ -15,3 +15,10 @@ def test_calculator_operations_subtract():
 def test_calculator_operations_multiply():
     """ Testing the Calculator operations multiply method """
     assert Multiplication.multiply(1.0, 2) == 2.0
+
+
+def test_calculator_operations_divide():
+    """ Testing the Calculator operations divide method """
+    assert Division.divide(1.0, 2) == 0.5
+    assert Division.divide(6.0, 2) == 3.0
+    assert Division.divide(1.0, 0) == "Cannot divide by 0"
