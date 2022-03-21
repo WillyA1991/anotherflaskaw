cd33456b3ddcdd80f19480ba52d0632a233ea687
Austin <awilson9654@gmail.com>
2022-03-21 17:51:02 -0400

    cleaned up some note language in calculator test file

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,35 +2,36 @@
 from calculator import Calculator
 
 
-def tuple_list():
+def tuple_list_1():
     """ Arrange data for AAA testing """
     return 1.0, 2
 
 def tuple_list_2():
-    """ Arrange data for AAA testing of bad division """
+    """ Arrange data for AAA testing of division by 0 """
     return 1.0, 0
 
 
 def test_calculator_add_method():
     """ Testing the calculator add method """
     # this is shown using the calculator object add method
-    result = Calculator.add(tuple_list())
+    result = Calculator.add(tuple_list_1())
     assert result == 3.0
 
 
 def test_calculator_subtract_method():
     """ Testing the calculator subtract method """
-    assert Calculator.subtract(tuple_list()) == -3.0
+    assert Calculator.subtract(tuple_list_1()) == -3.0
 
 
 def test_calculator_multiply_method():
     """ Testing the calculator multiply method """
-    assert Calculator.multiply(tuple_list()) == 2.0
+    assert Calculator.multiply(tuple_list_1()) == 2.0
 
 
 def test_calculator_divide_method_1():
     """ Testing the calculator divide method """
-    assert Calculator.divide(tuple_list()) == 0.5
+    assert Calculator.divide(tuple_list_1()) == 0.5
+
 
 def test_calculator_divide_method_2():
     """ Testing the calculator divide method with 0 as denominator """
