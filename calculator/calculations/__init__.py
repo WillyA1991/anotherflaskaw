f950996f7aff0ee01465a974e4bd94edc7f4fc88
Austin <awilson9654@gmail.com>
2022-03-23 19:04:29 -0400

    Pytest corrections by deleting an errant "z"

--- calculator/calculations/__init__.py
+++ calculator/calculations/__init__.py
@@ -1,7 +1,6 @@
 """Calculation and Addition, Multiplication, and Subtraction Classes"""
 from calculator.operations import Addition as Add, Subtraction as Sub, Multiplication as Mult, \
     Division as Div
-Z
 
 class Calculation:
     """calculation abstract base class"""
