aae9f64a7df0337d205445b3505cae0d30a5f8a5
Austin <awilson9654@gmail.com>
2022-03-22 21:39:23 -0400

    began running pytest pylint cov and made fixes

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -1,3 +1,4 @@
+""" Controller file for Simple Pages HTML pages """
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
@@ -8,6 +9,7 @@ simple_pages = Blueprint('simple_page', __name__,
 @simple_pages.route('/', defaults={'page': 'index'})
 @simple_pages.route('/<page>')
 def show(page):
+    """function for properly swapping pages in Simple Pages"""
     try:
         return render_template('%s.html' % page)
     except TemplateNotFound:
